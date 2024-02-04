00875e499cbdaf53be2fab94cfff6b18393c579e
norm02 <richa0920@gmail.com>
2024-02-05 07:17:32 +0900

    Add router & details component

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,9 @@ import {
   provideProtractorTestingSupport,
 } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
+import { provideRouter } from "@angular/router";
+import routeConfig from "./app/routes";
 
 bootstrapApplication(AppComponent, {
-  providers: [provideProtractorTestingSupport()],
+  providers: [provideProtractorTestingSupport(), provideRouter(routeConfig)],
 }).catch((err) => console.error(err));
