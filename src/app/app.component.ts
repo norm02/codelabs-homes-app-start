8620b57daffc495ac8869a08bbfa9e1d439d5ff8
norm02 <richa0920@gmail.com>
2024-02-04 20:31:29 +0900

    Fix code according to part3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
-import { HomeComponent } from "../home/home.component";
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
+  imports: [HomeComponent],
   template: `
     <main>
       <header class="brand-name">
@@ -20,7 +21,6 @@ import { HomeComponent } from "../home/home.component";
     </main>
   `,
   styleUrls: ["./app.component.css"],
-  imports: [HomeComponent],
 })
 export class AppComponent {
   title = "homes";
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,9 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { HousingLocationComponent } from "../housing-location/housing-location.component";
-
 @Component({
   selector: "app-home",
   standalone: true,
+  imports: [CommonModule],
   template: `
     <section>
       <form>
@@ -12,11 +11,7 @@ import { HousingLocationComponent } from "../housing-location/housing-location.c
         <button class="primary" type="button">Search</button>
       </form>
     </section>
-    <section class="results">
-      <app-housing-location></app-housing-location>
-    </section>
   `,
   styleUrls: ["./home.component.css"],
-  imports: [CommonModule, HousingLocationComponent],
 })
 export class HomeComponent {}
